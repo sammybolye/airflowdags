9bc2de048e9316f35c77d22d4c929ff331d6690c
sammybolye <58224695+sammybolye@users.noreply.github.com>
2024-04-07 15:42:44 +0100

    Update sparkop.py

--- dags/sparkop.py
+++ dags/sparkop.py
@@ -31,10 +31,12 @@ from datetime import timedelta, datetime
 # The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
 # Operators; we need this to operate!
+from airflow.operators.bash_operator import BashOperator
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
+from airflow.providers.cncf.kubernetes.hooks.kubernetes import KubernetesHook
 from airflow.utils.dates import days_ago
-
+k8s_hook = KubernetesHook(conn_id='kubernetes_default')
 # [END import_module]
 
 # [START default_args]
@@ -68,7 +70,6 @@ submit = SparkKubernetesOperator(
     task_id='spark_pi_submit',
     namespace="sampletenant",
     application_file="new_spark_pi.yaml",
-    kubernetes_conn_id="kubernetes_default",
     do_xcom_push=True,
     dag=dag,
     api_group="sparkoperator.hpe.com",
@@ -79,7 +80,6 @@ sensor = SparkKubernetesSensor(
     task_id='spark_pi_monitor',
     namespace="sampletenant",
     application_name="{{ task_instance.xcom_pull(task_ids='spark_pi_submit')['metadata']['name'] }}",
-    kubernetes_conn_id="kubernetes_default",
     dag=dag,
     api_group="sparkoperator.hpe.com",
     attach_log=True
